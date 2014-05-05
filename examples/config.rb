41e542f31f3f58c4b0c7d87d68771b5618e6eb22
Sam Richard <snugug@gmail.com>
2014-05-05 09:17:42 -0400

    Updated Examples
    Examples now come from North generator

--- examples/config.rb
+++ examples/config.rb
@@ -1,9 +1,13 @@
 # Require any additional compass plugins here.
-
-add_import_path "../north"
+require 'compass/import-once/activate'
+require 'breakpoint'
+require 'singularitygs'
+require 'toolkit'
+require 'modular-scale'
+require 'color-schemer'
 
 # Set this to the root of your project when deployed:
-http_path = "/"
+http_path = "./"
 css_dir = "css"
 sass_dir = "sass"
 images_dir = "images"
@@ -18,10 +22,3 @@ relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
-
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
